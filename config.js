ab90b8e4d369e07d117f1900f5e2b7c5fe83a7fd
funsteamleones <funsteamleones@users.noreply.github.com>
2026-06-12 17:02:13 -0300

    Set Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 // IMPORTANTE: Reemplaza estos valores con los que obtengas de tu proyecto de Supabase (Project Settings -> API)
-const SUPABASE_URL = 'TU_SUPABASE_URL_AQUI';
+const SUPABASE_URL = 'https://sbvcicqpaljcjgbpwghe.supabase.co';
 const SUPABASE_ANON_KEY = 'TU_SUPABASE_ANON_KEY_AQUI';
 
 window.supabaseConfig = {
