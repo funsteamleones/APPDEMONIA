f785e62e3590c9a255745697af6cad760c5cca53
funsteamleones <funsteamleones@users.noreply.github.com>
2026-06-12 17:04:06 -0300

    Set Supabase Anon Key

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 // IMPORTANTE: Reemplaza estos valores con los que obtengas de tu proyecto de Supabase (Project Settings -> API)
 const SUPABASE_URL = 'https://sbvcicqpaljcjgbpwghe.supabase.co';
-const SUPABASE_ANON_KEY = 'TU_SUPABASE_ANON_KEY_AQUI';
+const SUPABASE_ANON_KEY = 'sb_publishable_qlqLdiHXeaoXjrn_FzAshw_ZZ_jMbDp';
 
 window.supabaseConfig = {
     url: SUPABASE_URL,
